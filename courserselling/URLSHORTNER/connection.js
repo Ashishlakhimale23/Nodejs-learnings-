43962e376859b1d1a4fcb8abd37345356d006e37
Ashishlakhimale23 <124334273+Ashishlakhimale23@users.noreply.github.com>
2024-02-05 22:07:59 +0530

    Delete courserselling/URLSHORTNER directory

--- courserselling/URLSHORTNER/connection.js
+++
@@ -1,6 +0,0 @@
-const mongoose=require("mongoose");
-async function connectdb(url){
-    return await mongoose.connect(url);
-}
-
-module.exports=connectdb;
--- courserselling/URLSHORTNER/routes/user.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const userrouter = express.Router();
-const {handlersignup,
-    handlerlogin}=require("../controllers/user")
-
-userrouter.post("/signup",handlersignup);
-userrouter.get("/login",handlerlogin);
-
-module.exports = userrouter;
